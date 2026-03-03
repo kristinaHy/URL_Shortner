50477670c1a837702491e9f247f17887a5d6bcf0
Kristina Hyaunmkiha <hyaunmikhakristina@gmail.com>
2026-03-03 14:59:50 +0545

    Added templates and views also

--- shortener/views.py
+++ shortener/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import URL
 
 def home(request):
+    short_url = None
+
     if request.method == "POST":
         long_url = request.POST.get("long_url")
 
         url_obj = URL.objects.create(long_url=long_url)
 
-        return render(request, "shortener/home.html", {
-            "short_code": url_obj.short_code
-        })
+        short_url = request.build_absolute_uri("/") + url_obj.short_code
 
-    return render(request, "shortener/home.html")
+    return render(request,"shortener/home.html",{
+        "short_url": short_url
+    })
 
 def redirect_url(request, code):
     url = get_object_or_404(URL, short_code=code)
