68eddd3dd0463a511412c6676d3604b2b15deb75
Kristina Hyaunmkiha <hyaunmikhakristina@gmail.com>
2026-03-03 14:46:30 +0545

    Add URL model

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+
 
 # Create your models here.
+import uuid
+from django.db import models
+
+class URL(models.Model):
+    long_url = models.URLField()
+    short_code = models.CharField(max_length=10, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.short_code:
+            self.short_code = str(uuid.uuid4())[:6]
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.long_url
