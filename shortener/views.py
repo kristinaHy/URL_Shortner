08ec960d56e953113c1d09ae614facdf1ee89657
Kristina Hyaunmkiha <hyaunmikhakristina@gmail.com>
2026-03-03 14:48:29 +0545

    Add short URL creation view

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import URL
 
-# Create your views here.
+def home(request):
+    if request.method == "POST":
+        long_url = request.POST.get("long_url")
+
+        url_obj = URL.objects.create(long_url=long_url)
+
+        return render(request, "shortener/home.html", {
+            "short_code": url_obj.short_code
+        })
+
+    return render(request, "shortener/home.html")
